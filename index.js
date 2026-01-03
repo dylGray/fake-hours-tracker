ade18ef0872444e26f231fe7c6652efb8654b7f4
Dylan Gray <dylan.gray@revenuepathgroup.com>
2026-01-03 15:32:41 -0500

    working on parsing user data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 // `Client` is the main class for interacting with the Discord API
 // `GatewayIntentBits` allows us to list all the events our bot can listen for
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, messageLink, Guild } = require("discord.js");
 
 // this bot will only get events specified in our new client object
 const client = new Client({
@@ -16,23 +16,41 @@ const client = new Client({
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
+
+    let voiceChannels = {
+        shooters: null,
+        cornFields: null,
+    };
+
+    // finding all servers (guilds) that our bot lives in
+    client.guilds.cache.forEach(guild => {
+        voiceChannels.shooters = guild.channels.cache.find(channel => channel.id === process.env.SHOOTERS_VC_ID);
+        voiceChannels.cornFields = guild.channels.cache.find(channel => channel.id === process.env.CORN_FIELDS_VC_ID);
+    });
+
+    const benny = voiceChannels.shooters.members.get('825977188339548210');
+    console.log(benny.user.username);
+
 });
+
 
 // this runs every time a message is created in a channel
 // the message param is an object containing message meta-data
-client.on('messageCreate', async(message) => {
-    if (message.author.bot) return;
+// client.on('messageCreate', async(message) => {
+//     if (message.author.bot) return;
 
-    const guild = message.guild;
+//     const guild = message.guild;
 
-    if (message.content.trim().toLowerCase() === '!ping') {
-        // const channelNames = guild.channels.cache.map(channel => channel.name).join(', ');
-        // let voiceChannels = [];
+//     if (message.content.trim().toLowerCase() === '!ping') {
+//         // const channelNames = guild.channels.cache.map(channel => channel.name).join(', ');
+//         // let voiceChannels = [];
 
-        console.log(guild.channels.cache);
+//         console.log(guild.channels.cache);
 
-        await message.reply(`Pong!\n${voiceChannels}`);
-    }
-});
+//         await message.reply(`Pong!\n${guild.channels.cache}`);
+//     }
+// });
+
+
 
 client.login(process.env.DISCORD_TOKEN);
